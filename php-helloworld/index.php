6fff57b553ce3859808e9784627a9cf5eb7144a1
Student User <student@workstation.lab.example.com>
2022-02-23 02:52:16 -0500

    changed index file

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! Welcome to RHOCP....,  php version is " . PHP_VERSION . "\n";
+print "Hello, World! Welcome to RHOCP....,Manikandan  php version is " . PHP_VERSION . "\n";
 ?>
